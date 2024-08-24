60e0f7873c5ddc5d76e801e00b7500bf113dd259
Edgar Hernando Diaz <edgarh2dm3@gmail.com>
2024-08-24 16:46:26 -0500

    ViewChild - ElementRef

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,13 +1,11 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
-import  mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import  { Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 // No Hice
 //import * as mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 //npm i --save-dev @types/mapbox-gl
 
-
-(mapboxgl as any).accessToken = 'pk.eyJ1IjoiZWFnbGVoZG0yIiwiYSI6ImNtMDM3bGF3ajA5NDkyaW9odWxyNHdvNWEifQ.gnTl5SxPbHZmLUCOPl8NBw';
 
 @Component({
   templateUrl: './full-screen-page.component.html',
@@ -15,11 +13,14 @@ import  mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');
 })
 export class FullScreenPageComponent implements AfterViewInit {
 
-
+@ViewChild ('map') divMap?: ElementRef;
 
   ngAfterViewInit(): void {
-    const map = new mapboxgl.Map({
-      container: 'map', // container ID
+
+    if (!this.divMap) throw 'El elemento HTML no fue encontrado';
+
+    const map = new Map({
+      container: this.divMap.nativeElement, // referncia local #map
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
       zoom: 9, // starting zoom
